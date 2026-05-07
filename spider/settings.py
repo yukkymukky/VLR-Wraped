dc55fb6071f5db7d74855ba3472d96d046af7e93
numanumanuman <183443524+yukkymukky@users.noreply.github.com>
2026-05-06 20:21:34 -0700

    Change HTTP Expiration to 10 mins

--- spider/settings.py
+++ spider/settings.py
@@ -79,7 +79,7 @@ DOWNLOADER_MIDDLEWARES = {
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
 HTTPCACHE_ENABLED = True
-HTTPCACHE_EXPIRATION_SECS = 60
+HTTPCACHE_EXPIRATION_SECS = 600
 HTTPCACHE_DIR = "httpcache"
 HTTPCACHE_IGNORE_HTTP_CODES = []
 HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
